f4f8843ae6b8f0ce8fb8d7b0be3cc6d2f791f545
KNNsoftuni <Konstantin.nikolov.tino@gmail.com>
2023-12-11 00:05:31 +0200

    Stacks and Queues EXERCISES
    Stacks and queues exercises. Song-Queue is not functional.

---
+++ StacksQueueEXercises/Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> boxOfClothes = new Stack<int>();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            foreach (int item in input.Reverse())
+            {
+                boxOfClothes.Push(item);
+            }
+            
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int currentSum = 0;
+            int rackCount = 1;
+
+            while (boxOfClothes.Count > 0)
+            {
+                if (currentSum + boxOfClothes.Peek() <= rackCapacity)
+                {
+                    currentSum += boxOfClothes.Pop();
+                }
+                else
+                {
+                    currentSum = boxOfClothes.Pop();
+                    rackCount++;
+                }
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
---
+++ StacksQueueEXercises/basicQueueOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace basicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int S = input[1];
+            int X = input[2];
+            Queue<int> numQueue = new Queue<int>();
+            List<int> enqueNum = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            enqueNum.ForEach(x => numQueue.Enqueue(x));
+
+            for (int i = 0; i < S; i++)
+            {
+                numQueue.Dequeue();
+            }
+
+            if (numQueue.Count() > 0)
+                if (numQueue.Contains(X))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(numQueue.Min());
+                }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
